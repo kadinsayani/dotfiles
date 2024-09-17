b99541476688f5bf4d628ccc3c09d56c235db8ff
Kadin Sayani <kadin.sayani@canonical.com>
2024-09-17 15:24:33 -0600

    Updated nvim config
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,9 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4 -- Shift 4 spaces when tab
-vim.opt.tabstop = 4 -- 1 tab == 4 spaces
+vim.opt.expandtab = false -- Use spaces instead of tabs
+-- vim.opt.shiftwidth = 4 -- Shift 4 spaces when tab
+-- vim.opt.tabstop = 4 -- 1 tab == 4 spaces
 vim.opt.smartindent = true -- Autoindent new lines
 vim.opt.wrap = true
 vim.opt.linebreak = true
